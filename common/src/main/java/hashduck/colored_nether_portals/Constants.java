c4ac6f6be151b36a82b9f25dbc38a579df65f166
HashDuckZ <HashDuckZ@proton.me>
2026-04-03 15:32:31 +1000

    Setup project

---
+++ common/src/main/java/hashduck/colored_nether_portals/Constants.java
@@ -0,0 +1,11 @@
+package hashduck.colored_nether_portals;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+    public static final String MOD_ID = "colored_nether_portals";
+    public static final String MOD_NAME = "Colored Nether Portals";
+    public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
---
+++ common/src/main/java/hashduck/colored_nether_portals/mixin/MixinMinecraft.java
@@ -0,0 +1,18 @@
+package hashduck.colored_nether_portals.mixin;
+
+import hashduck.colored_nether_portals.Constants;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+
+    @Inject(at = @At("TAIL"), method = "<init>")
+    private void init(CallbackInfo info) {
+        Constants.LOG.info("This line is printed by the Colored Nether Portals common mixin!");
+        Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
+    }
+}
